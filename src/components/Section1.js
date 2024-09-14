822df534071938a564a7e55def9fd7affc3150a1
GamzKahraman <168836540+GamzKahraman@users.noreply.github.com>
2024-09-14 17:22:11 +0300

    Update Section1.js
    Z-Index ayarı yaptım

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
-export const Section1 = () => {
-    const [isContunue, setIscontunue] = useState(false);
 
-    const Section1 = () => {
-        return (
-            <section className="linear-gradient">
-                <div className="flex flex-col items-center justify-center h-full w-full first-section-background bg-center bg-cover">
-                    
+import React, { useState } from "react";  
+import { TypeAnimation } from 'react-type-animation';  
 
-                <TypeAnimation  
-        sequence={[  
-          "<Welcome My />\n<Portolio/>",  
-          0,  
-          () => {  
-            setIsContinue(true);  
-          },  
-        ]}  
-        speed={50} 
-        deletionspeed={60} 
-        repeat={0}   
-      />  
-</div>
-            </section>
-        );
-    };
-}
+export const Section1 = () => {  
+  const [isContinue, setIsContinue] = useState(false);  
+
+  return (  
+    <section className="linear-gradient">  
+      <div className="flex flex-col items-center justify-center h-full w-full first-section-background bg-center bg-cover">  
+        <TypeAnimation  
+          sequence={[  
+            "<Welcome My />\n<Portfolio/>",   
+            2000,   
+            () => {  
+              setIsContinue(true);  
+            },  
+          ]}  
+          speed={50}   
+          deletionSpeed={60} 
+          repeat={0}  
+        />  
+      </div>  
+    </section>  
+  );  
+};
+
+
 
 
 
