bd20d07d469ace302e163d836b872767f0f2b357
GamzKahraman <168836540+GamzKahraman@users.noreply.github.com>
2024-09-14 17:17:50 +0300

    Update App.js
    Yazdığım Section1 componentini App.js dosyası içerisinde kullandım

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+
 import { Navbar } from "./components/Navbar";
+import { Section1 } from './components/Section1';
 
 function App() {
  return (
   <div className="App">
-   <Navbar />
+   <Navbar/>
    Gamze
+   <Section1/>
   </div>
  );
 }
 
 export default App;
+
+ 
+ 
+
+
+
+
