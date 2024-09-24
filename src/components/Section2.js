b3bde725f72c092dfbb46d2c495ff4bc4972010c
GamzKahraman <168836540+GamzKahraman@users.noreply.github.com>
2024-09-24 17:53:57 +0300

    Update Section2.js
    projenin ikinci componenti olan "About" kısmı bitti

---
+++ src/components/Section2.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+export const Section2 = () => {
+    const labelClass =
+        "text-justify my-1 mx-1 text-[1.5vh] sm:text-[2vh] md::w-[40vw] xl:text-[2.5vh] bg-[#AEF359] p-2 text-center"
+
+    return (
+        <section className="bg-white text-white flex-col" id="section2">
+            {/* Arka Plan */}
+            <div className="w-full h-[100px] bg-[#AEF359]"></div>
+            <div className="w-full h-[90vh] flex flex-col  justify-start sm:justify-center mx-4">
+                <div className="flex items-center h-[15vh] justify-center ">
+                    {/* Metinlerin Css Özellikleri */}
+                    <label className="text-center text-white bg-[#AEF359] shadow-2xl font-extrabold text-[3vh] sm:text-[3.5vh] lg:text-[4.3vh] h-fit xl:mb-5 px-2">{`About Me`}</label>
+                </div>
+                <div className="flex flex-col md:flex-row md:justify-around">
+                    <label className={labelClass}>
+                    Hello, I'm Gamze. I was born in Konya and I live in Konya.
+                    I am currently a high school student preparing for university. 
+                    I started software as a volunteer at Kapsül Technology Platform, 
+                    first learning the JavaScript language and trying to improve myself
+                    with projects and learn new languages.
+                    </label>
+                </div>
+            </div>
+        </section>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
