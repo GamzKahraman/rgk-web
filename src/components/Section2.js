51a83f6d94127bf75d78d8b88fc533cd48a009ff
GamzKahraman <168836540+GamzKahraman@users.noreply.github.com>
2024-12-07 15:01:21 +0300

    Update Section2.js

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,26 +1,35 @@
 import React from "react";
 
 
+
 export const Section2 = () => {
+
     const labelClass =
-        "text-justify my-2 mx-2 text-[1.5vh] sm:text-[2vh] md::w-[40vw] xl:text-[2.5vh] bg-[#06141B] p-2 text-center";
+        "text-justify my-2 mx-2 text-[1.5vh] sm:text-[2vh] md::w-[40vw] xl:text-[2.5vh] bg-[#06141B] p-4 text-center max-w-[600px] mx-auto";
 
     return (
         <section className="bg-white text-white flex flex-col" id="section2">
             {/*Arka Plan*/}
             <div classsName="w-full h-[100px] bg-[#06141B]"></div>
             <div className="w-full h-[90vh] flex flex-col justify-start sm:justify-center mx-4">
-            <div className="flex flex-col items-center h-full justify-center">
-                {/*Metinlerin CSS Özellikleri*/}
-                <label className="text-center text-white bg-[#06141B] shadow-2xl font-extrabold text-[3vh] sm:text-[3.5vh] lg:text-[4.3vh] h-fit xl:mb-5 px-2" >
-                    {`ABOUT ME`}
+                <div className="flex flex-col items-center h-full justify-center">
+                    {/*Metinlerin CSS Özellikleri*/}
+                    <label className="text-center text-white bg-[#06141B] shadow-2xl font-extrabold text-[3vh] sm:text-[3.5vh] lg:text-[4.3vh] h-fit xl:mb-5 px-2" >
+                        {`ABOUT ME`}
                     </label>
 
                     <div className="flex flex-col md:flex-row md:justify-around">
-                    <label className={labelClass}>
-                    LOADİNG...
+                        <label className={labelClass}>
+                            Hello, I am Gamze, I am a 3rd year high school student.
+                            From the time I started high school, I decided that I was
+                            a versatile person, because I like to gain knowledge by doing
+                            research on all kinds of fields. One of these areas was web
+                            software. After my teacher noticed me after the 10-week training
+                            I received through the private teaching institution of the city
+                            I live in, I entered the Capsule technology platform voluntarily
+                            to improve myself, and I am still learning new things here as a volunteer.
 
-                    </label>
+                        </label>
                     </div>
                 </div>
 
