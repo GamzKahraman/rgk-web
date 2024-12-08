a83f6fcb406d9469c0b00d4f798be33527c02bb0
GamzKahraman <168836540+GamzKahraman@users.noreply.github.com>
2024-12-08 23:54:50 +0300

    Update Section2.js

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -5,16 +5,16 @@ import React from "react";
 export const Section2 = () => {
 
     const labelClass =
-        "text-justify my-2 mx-2 text-[1.5vh] sm:text-[2vh] md::w-[40vw] xl:text-[2.5vh] bg-[#06141B] p-4 text-center max-w-[600px] mx-auto";
+        "text-justify my-2 mx-2 text-[1.5vh] sm:text-[2vh] md:w-[40vw] xl:text-[2.5vh] p-4 text-center max-w-[600px] mx-auto bg-[#ADBC9F]";
 
     return (
-        <section className="bg-white text-white flex flex-col" id="section2">
+        <section className="bg-[#12372A] text-white flex flex-col" id="section2">
             {/*Arka Plan*/}
-            <div classsName="w-full h-[100px] bg-[#06141B]"></div>
+            <div classsName="w-full h-[100px] bg-[#12372A]"></div>
             <div className="w-full h-[90vh] flex flex-col justify-start sm:justify-center mx-4">
                 <div className="flex flex-col items-center h-full justify-center">
                     {/*Metinlerin CSS Özellikleri*/}
-                    <label className="text-center text-white bg-[#06141B] shadow-2xl font-extrabold text-[3vh] sm:text-[3.5vh] lg:text-[4.3vh] h-fit xl:mb-5 px-2" >
+                    <label className="text-center text-white bg-[#ADBC9F] shadow-2xl font-extrabold text-[3vh] sm:text-[3.5vh] lg:text-[4.3vh] h-fit xl:mb-5 px-2" >
                         {`ABOUT ME`}
                     </label>
 
