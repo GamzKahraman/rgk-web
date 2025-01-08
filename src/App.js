dc0adede5161f122ba562d8845135d73f1325494
GamzKahraman <168836540+GamzKahraman@users.noreply.github.com>
2025-01-08 16:58:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import './index.css';
 
+
 import { Navbar } from "./components/Navbar";
 import { Section1 } from './components/Section1';
 import { Section2 } from "./components/Section2";
@@ -17,6 +18,8 @@ function App() {
    <Section2 />
    <Section3 />
    <Section4/>
+  
+ 
 
    
   </div>
